2e89645882631a03a6fa272583fbf1a59c380410
VangavarapuAnitha <sscanitha123@gmail.com>
2023-11-27 20:24:59 +0530

    show status message to the user and calculate winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,6 @@
 import Square from './Square';
-import { useState } from 'react';
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXnext] = useState(false);
-  const handleSquareClick = clickedPosition => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : '0';
-        }
-        return squareValue;
-      });
-    });
-    setIsXnext(currentIsXNext => !currentIsXNext);
-  };
+
+const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
